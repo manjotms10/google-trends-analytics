1d810c8e0bd657840c9fb5a5223bf01623daf789
Tushar Dobhal <tdobhal@eng.ucsd.edu>
2019-11-10 12:29:15 -0800

    Added code for multiple keywords search in GoogleTrends class

--- src/examples/google_trends_example.py
+++ src/examples/google_trends_example.py
@@ -1,9 +1,9 @@
 from utils.google_trends import GoogleTrends
 
-keywords = ['jennifer', 'aniston']
-start_date = '2018-09-09'
-end_date = '2019-09-09'
+keywords = ['avengers', 'endgame', 'lion king', 'lion', 'toy story', 'woody', 'captain marvel', 'marvel', 'spider man', 'homecoming']
+start_date = '2019-01-01'
+end_date = '2019-11-09'
 
 gt = GoogleTrends()
 
-gt.get_trends_data(keywords=keywords, start_date=start_date, end_date=end_date).plot(save_fig=True)
+gt.get_trends_data_from_multiple_keywords(keywords=keywords, start_date=start_date, end_date=end_date).plot(save_fig=True)
