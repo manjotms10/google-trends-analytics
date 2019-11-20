23e0c4d77896b1b121ffdfb14e33a244f25975fe
manjotms10 <manjotms10@gmail.com>
2019-11-20 11:38:51 -0800

    [Movies]: Add genre plots

--- src/analytics/movies/comparison.py
+++ src/analytics/movies/comparison.py
@@ -65,7 +65,7 @@ def box_office_by_year(query_year, google_trends, imdb_parser, path):
 
     # Create a dataframe to hold results and compare.
     df = pd.DataFrame(data, columns = ["pytrends", "imdb"], index=movies)
-    
+
     # Normalize the columns
     cols_to_norm     = ['pytrends','imdb']
     df[cols_to_norm] = df[cols_to_norm].apply(
@@ -80,14 +80,103 @@ def box_office_by_year(query_year, google_trends, imdb_parser, path):
     fig.savefig(path + f'{query_year}_plot_{str(np.random.randint(1, 10))}.png')
 
 
+def box_office_by_genre(genre, metric, google_trends, imdb_parser, path):
+    '''
+    Plot Box Office Collections vs Google Trends by year.
+
+    Parameters
+    ----------
+    query_year:    A valid year between 2010 and 2019, passed as a string.
+    google_trends: A valid instance of GoogleTrends class.
+    imdb_parsed:   A valid instance of IMDBWebScraper class.
+    path:          A valid path to save the resultant plot.
+
+    Returns
+    -------
+    None
+
+    Example:
+    >>> google_trends = GoogleTrends()
+    >>> imdb_parser = IMDBWebScraper()
+    >>> path = "../../../plots/movies/"
+    >>> box_office_by_genre("action", google_trends, imdb_parser, path)
+    '''
+
+    assert isinstance(genre, str)
+    assert isinstance(metric, str)
+    assert isinstance(google_trends, GoogleTrends)
+    assert isinstance(imdb_parser, IMDBWebScraper)
+    assert isinstance(path, str)
+
+    # Get the result from box office collection parser.
+    box_office_df  = imdb_parser.get_top_movies_by_genre(genre = genre, metric = metric)
+    idx            = np.random.choice(range(box_office_df.shape[0]), 5)
+    
+    movies         = list(box_office_df.iloc[idx]["name"])
+    start_date     = "2010-01-01"
+    end_date       = "2019-12-31"
+
+    # Query pytrends
+    try:
+        google_trends_box_office_df = \
+            google_trends.get_trends_data_from_multiple_keywords(
+                                        keywords   = movies,
+                                        start_date = start_date,
+                                        end_date   = end_date).data
+    except:
+        print("Exception caught with data-loader. Returning ...")
+        return
+
+    # Parse result from Box Office Collections (str).
+    box_office_collections = list(box_office_df.iloc[idx]["gross"])
+    imdb_votes             = list(box_office_df.iloc[idx]["votes"])
+
+    for i in range(len(imdb_votes)):
+        imdb_votes[i] = ''.join(imdb_votes[i].split(','))
+
+    # Sum all the queries across the year.
+    total_queries = google_trends_box_office_df.sum(axis = 0)
+
+    data = []
+    for movie, val, votes in zip(movies, box_office_collections, imdb_votes):
+        # TODO: Check if movie is a valid key.
+        data.append((np.float32(total_queries[movie]),\
+                                np.float32(val),\
+                                np.float32(votes)))
+
+    # Create a dataframe to hold results and compare.
+    df = pd.DataFrame(data, columns = ["pytrends", "gross", "votes"], index=movies)
+
+    # Normalize the columns
+    cols_to_norm     = ['pytrends','gross','votes']
+    df[cols_to_norm] = df[cols_to_norm].apply(
+                        lambda x: (x - x.min()) / (x.max() - x.min()))
+
+    # ------ View Plot ------
+    # df.plot.bar(y = ['votes','gross'], rot=90, figsize=(80, 10))
+    # plt.legend(["pytrends", "imdb"])
+    # plt.show()
+
+    # Plot the comparison and save the result.
+    fig = df.plot.bar(y = ['votes','gross'], rot=0, figsize=(60, 10)).get_figure()
+    plt.legend(["pytrends", "imdb"])
+    fig.savefig(path + f'{genre}_plot_{str(np.random.randint(1, 10))}.png')
+    
+
 if __name__ == '__main__':
     # Create data loaders for Google Trends and IMDBWebScraper
     google_trends   = GoogleTrends()
     imdb_parser     = IMDBWebScraper()
     path            = ""
 
+    for genre in ["action", "comedy", "Horror", "Sci-Fi", "Drama", "Romance"]:
+        try:
+            box_office_by_genre(genre, "num_votes", google_trends, imdb_parser, path)
+        except:
+            print("Assertion hit with genre = ", genre)
+
     for year in range(2010, 2019):
-        #try:
-        box_office_by_year(str(year), google_trends, imdb_parser, path)
-        # except:
-        #     print("Exception occured with year = ", year, "\n")
+        try:
+            box_office_by_year(str(year), google_trends, imdb_parser, path)
+        except:
+            print("Exception occured with year = ", year, "\n")
